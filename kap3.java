90536035f27dc550183512c5a5d5a8c49668a38b
Daniel Carlsson <cada16@nti.umea>
2017-09-25 12:42:41 +0200

    slut klart

---
+++ kap3.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+/*	
+	Jag vet inte
+	Daniel Carlsson
+	2017-09-25
+*/
+	public class kap3 {
+		public static void main(String[] args) {
+
+			int i;
+			i = "500.7\n";
+			double d;
+			d = "500.7\n";
+			byte b;
+			b = "500.7\n";
+
+			//jag har inte lärt mig mycket den här lektionen, cmd sa att ingen av "talen" fungerade på varken 500 eller 500.7
+			//jag inser att int inte fungerar pga den kan inte ta högre än 127 och float&double har för hög "avrundning" men jag förstod inte cmd...
+
+		}
+	}
